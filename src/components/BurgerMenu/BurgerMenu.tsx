67d311e443202febb40a057cb602e0cfdfbc7d8d
Oleksandr-Berd <alex.berd86@gmail.com>
2023-07-22 19:34:53 +0300

    replace any in handleChange

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,26 +1,28 @@
-import {  MenuItem } from "@blueprintjs/core";
-
-import * as SC from "./BurgerMenuStyled"
+import { MenuItem } from "@blueprintjs/core";
+import * as SC from "./BurgerMenuStyled";
 import { useNavigate } from "react-router-dom";
 import { IBurgerMenuProps } from "../../utils/interfaces";
 
+const BurgerMenu: React.FC<IBurgerMenuProps> = ({ handleMenu }): JSX.Element => {
+    const navigate = useNavigate();
 
-const BurgerMenu: React.FC<IBurgerMenuProps> = ({handleMenu}): JSX.Element => {
-    const navigate = useNavigate()
+    const handleClick = (evt: React.MouseEvent<HTMLElement>): void => {
+        const target = evt.currentTarget as HTMLElement;
+        const text = target.textContent;
+        const path = text === "home" ? "/" : text === "contact me" ? "contact" : "portfolio";
+        handleMenu();
+        navigate(path);
+    };
 
-    const handleClick = (evt: any): void => {
-        const path = evt.currentTarget.text === "home" ? "/" : evt.currentTarget.text === "contact me" ? "contact" : "portfolio"
-        handleMenu()
-        navigate(path)
-    }
-
-    return (<div>
-        <SC.MenuStyled>
-            <MenuItem onClick={handleClick} text="home" />
-            <MenuItem onClick={handleClick} text="portfolio" />
-            <MenuItem onClick={handleClick} text="contact me" />
-        </SC.MenuStyled>
-    </div>);
-}
+    return (
+        <div>
+            <SC.MenuStyled>
+                <MenuItem onClick={handleClick} text="home" />
+                <MenuItem onClick={handleClick} text="portfolio" />
+                <MenuItem onClick={handleClick} text="contact me" />
+            </SC.MenuStyled>
+        </div>
+    );
+};
 
 export default BurgerMenu;
