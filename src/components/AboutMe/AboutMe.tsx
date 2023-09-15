b866179d3c79ddb2400e18302eba6a03f2ab9cd7
Oleksandr-Berd <alex.berd86@gmail.com>
2023-09-15 13:25:21 +0300

    fix about me

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -24,11 +24,10 @@ const AboutMe: React.FC = (): JSX.Element => {
                         <p> 👨‍💻 Imagine a passionate developer who dances effortlessly with the MERN (MongoDB, Express.js, React, Node.js) tech stack. Well, that's me!</p>
                         <p>🚀 With one year of rocket-fueled experience in MERN, I've been unleashing creativity on both the client and server sides. Building captivating web applications is my jam!</p>
                         <p> 💡 Are you ready for innovation? I'm on the lookout for thrilling collaborations with like-minded programmers. Together, we'll harness the power of MERN to create the next big thing!</p>
-                        <p> 🤝 Whether it's a startup project or a daring venture, count me in! Join me on this coding journey, and let's forge remarkable solutions that'll leave everyone awestruck.</p>
+                        <p> 🤝 Whether it's a startup project or a daring venture, count me in!</p>
                         <p> 🔥 Passion, proficiency, and a pinch of pizzazz – that's what I bring to the table. Let's connect and weave lines of code into sheer awesomeness!</p>
                         <p>
                             Ready to take the plunge into the world of MERN magic? Let's code our way to greatness!
-                            Go ahead and add your own flair to the text, highlighting your unique skills and personality. Remember, your "About Me" section is your chance to make a memorable impression on potential collaborators, so make it shine! If you have any more requests or need further help, feel free to ask. Happy coding!
                             </p>
                     </SC.TextAccordion>
                 </SC.AccordionItem>
